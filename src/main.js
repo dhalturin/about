01b86378f35fb49510fed2fa84e95d28cf0f9862
Denis Halturin <dhalturin@hotmail.com>
2023-12-22 20:45:55 +0400

    added ga
    Signed-off-by: Denis Halturin <dhalturin@hotmail.com>

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import VueCalendarHeatmap from 'vue3-calendar-heatmap'
+import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +13,14 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueCalendarHeatmap)
+app.use(
+    VueGtag,
+    {
+        appName: 'about me',
+        pageTrackerScreenviewEnabled: true,
+        config: { id: 'G-04BMYM0H2Q' },
+    },
+    router,
+)
 
 app.mount('#app')
