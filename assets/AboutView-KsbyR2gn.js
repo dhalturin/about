1568207b7d90ab13c22a204b8c89b387e95cca6b
dhalturin <dhalturin@users.noreply.github.com>
2024-11-13 15:42:59 +0000

    deploy: 5a4325cdaa1e7e059d491ec26dda47a06d270889

--- assets/AboutView-KsbyR2gn.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as n,a,t as o,F as s,_ as r}from"./index-ulL5TSd8.js";function c(e,u){return t(),n(s,null,[a("p",null,o(e.$t("about_1",{tools:"Bash / Python / Jinja / Groovy / Golang / Vuejs"})),1),a("p",null,o(e.$t("about_2")),1)],64)}const l={},p=r(l,[["render",c],["__scopeId","data-v-ec9ce56f"]]);export{p as default};
